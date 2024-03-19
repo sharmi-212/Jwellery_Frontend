e8ebc6b51435a64d99d92487d498b643d58733ef
sharmi-212 <118599027+sharmi-212@users.noreply.github.com>
2024-03-19 20:58:49 +0530

    Update BookAdmin.js

--- src/components/BookAdmin/BookAdmin.js
+++ src/components/BookAdmin/BookAdmin.js
@@ -9,7 +9,7 @@ const BookAdmin = (props) => {
     const {_id, name,idno, description, price, image} = props.book;
     const deleteHandler = async () => {
         await axios
-          .delete(`https://https://home-appliances-be.onrender.com/books/${_id}`)
+          .delete(`https://jwellery-be.onrender.com/books/${_id}`)
           .then((res) => res.data)
           .then(() => window.location.reload())
           .then(() => history("/books"));
